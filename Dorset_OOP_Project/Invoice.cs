e8b0f6866d23af3e7e940441168f8d49265a4495
YoanGab <yoan.gabison@gmail.com>
2020-11-15 19:50:23 +0100

    Add methods to Invoice and Payment Entities

--- Dorset_OOP_Project/Invoice.cs
+++ Dorset_OOP_Project/Invoice.cs
@@ -1,10 +1,26 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace Dorset_OOP_Project
 {
     public class Invoice
     {
-        public Invoice()
+        public float Amount { get; set; }
+        public List<Payment> Payments { get; set; }
+        public Invoice(float amount, List<Payment> payments)
         {
+            Amount = amount;
+            Payments = payments;
+        }
+
+        public float OustandingBalance()
+        {
+            float total = Amount;
+            foreach(Payment payment in Payments)
+            {
+                total -= payment.Amount;
+            }
+            return total;
         }
     }
 }
--- Dorset_OOP_Project/Payment.cs
+++ Dorset_OOP_Project/Payment.cs
@@ -3,8 +3,22 @@ namespace Dorset_OOP_Project
 {
     public class Payment
     {
-        public Payment()
+        public float Amount { get; set; }
+        public Student Student { get; set; }
+        public Invoice Invoice { get; set; }
+        public DateTime Date { get; set; }
+
+        public Payment(float amount, Student student, Invoice invoice, DateTime date)
         {
+            Amount = amount;
+            Student = student;
+            Invoice = invoice;
+            Date = date;
+        }
+
+        public void Add()
+        {
+            Invoice.Payments.Add(this);
         }
     }
 }
