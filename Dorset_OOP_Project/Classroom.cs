5ede3979367753a2b6870cae1f00d5612b2c42bf
Baptiste <baptistegaidou@gmail.com>
2020-11-18 12:10:55 +0000

    Ajout de la class classroom, reste à implémenter dans  le reste du code, j'ai juste créer la classe et les attributs

---
+++ Dorset_OOP_Project/Classroom.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dorset_OOP_Project
+{
+    public class Classroom
+    {
+        public int ClassRoomID { get; set; }
+        public string ClassroomName{ get; set; }
+        public Faculty ClassRoomFaculty { get; set; }
+        public List<Student> ClassRoomStudents { get; set; }
+        public List<Discipline> ClassRoomDisciplines { get; set; }
+        public Classroom(string _classRoomName)
+        {
+            ClassroomName = _classRoomName;
+            ClassRoomStudents = new List<Student>();
+            ClassRoomDisciplines = new List<Discipline>();
+        }
+        public bool UpdateClassRoomEnrolment()
+        {
+            bool possible = false;
+            if (ClassRoomFaculty != null && ClassRoomStudents != null && ClassRoomStudents.Count != 0 && ClassRoomDisciplines != null && ClassRoomDisciplines.Count != 0)
+            {
+                possible = true;
+                for (int indexDiscipline = 0; indexDiscipline < ClassRoomDisciplines.Count; indexDiscipline++)
+                {
+                    ClassRoomDisciplines[indexDiscipline].EnrollAFaculty(ClassRoomFaculty);
+                    for(int indexStudent = 0; indexStudent < ClassRoomStudents.Count; indexStudent++)
+                    {
+                        ClassRoomDisciplines[indexDiscipline].EnrollAStudent(ClassRoomStudents[indexStudent]);
+                    }
+                }
+            }
+            return possible;
+        }
+        public string ClassRoomInformation()
+        {
+            string information = "";
+            information += $"Classroom ID : {ClassRoomID}\nClassroom name : {ClassroomName}\nFaculty teaching {ClassRoomFaculty.PublicApplicationInformation()}";
+            for (int indexStudent = 0; indexStudent < ClassRoomStudents.Count; indexStudent++)
+            {
+                information += $"\n{ClassRoomStudents[indexStudent].PublicApplicationInformation()}";
+            }
+            return information;
+        }
+    }
+}
